ae87f0d188bfb053b4f60a3163090596bec8bd24
ianflig <129326345+ianflig@users.noreply.github.com>
2025-12-12 12:57:24 -0300

    feat: done exercise

---
+++ js/script.js
@@ -0,0 +1,54 @@
+// hacer piedra papel o tijera con la compu, anotar un contador de puntos para el humano y para la computadora
+
+//Funcion random piedra papel o tijera
+let getComputerChoice = () => {
+    let value = Math.floor(Math.random() * 3);
+    return (value === 0) ? "piedra"
+        : (value === 1) ? "papel"
+        : "tijera";
+};
+
+
+//Funcion jugar cantidad de rondas
+function playGame(rounds){
+    let humanScore = 0;
+    let computerScore = 0;
+
+    //Funcion juego general
+    function gameRockPaperScissors(userChoice){
+        let computerChoice = getComputerChoice();
+        userChoice = userChoice.toLowerCase();
+        let result;
+
+        if (userChoice == computerChoice){
+            result = "Empate";
+        } else if (
+            (userChoice == "papel" && computerChoice == "piedra") || 
+            (userChoice == "tijera" && computerChoice == "papel") ||
+            (userChoice == "piedra" && computerChoice == "tijera")
+        ){
+            humanScore++;
+            result = "Ganaste";
+        } else {
+            computerScore++;
+            result = "Perdiste";
+        }
+
+        return `${result} - La computadora eligió ${computerChoice}
+        Puntajes
+
+        Computadora: ${computerScore}
+        Humano: ${humanScore}
+        `;
+    }
+
+    for (let i = 0; i<rounds; i++){
+        
+        //Cuadro petición al usuario y mostrar resultados
+        let inputUsuario = prompt(`Ronda ${i+1}: Elige piedra, papel o tijera`);
+        
+        alert(gameRockPaperScissors(inputUsuario));
+    }
+}
+
+playGame(5);
